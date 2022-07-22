d240891c3f566b33fcbcdb0302f03e96d4d5cf69
thanbv1510 <thanbv1510@gmail.com>
2022-07-22 14:44:37 +0700

    Initial Spring Boot project

---
+++ src/main/java/dev/thanbv1510/eventmonitoringsamplev2/EventMonitoringSampleV2Application.java
@@ -0,0 +1,18 @@
+package dev.thanbv1510.eventmonitoringsamplev2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMonitoringSampleV2Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMonitoringSampleV2Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/dev/thanbv1510/eventmonitoringsamplev2/EventMonitoringSampleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thanbv1510.eventmonitoringsamplev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMonitoringSampleV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
